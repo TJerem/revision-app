11d64dba41cc046ad22b7fa9d64425f6a2fc7bd9
tjerem <jeremy.tanasi@outlook.fr>
2026-07-07 16:06:26 +0200

    chore: initialisation du projet

---
+++ src/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+__version__ = "1.0.0"
+app = Flask(__name__)
+
+@app.get("/")
+def index():
+    return jsonify(message="Usine logicielle", version=__version__)
+
+@app.get("/version")
+def version():
+    return jsonify(version=__version__)
